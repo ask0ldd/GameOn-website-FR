e4f3dee44257ecd177168ad26759acd77b3cdec6
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-03-18 22:52:47 +0100

    validation functions

---
+++ formHandler.js
@@ -0,0 +1,86 @@
+// class errorNode
+class errorMessageNode {
+
+    constructor(nodeSelector) 
+    {
+        this.node = document.querySelector(nodeSelector)
+    }
+
+    show() 
+    { 
+        this.node.style.display="block" 
+    }
+
+    hide() 
+    { 
+        this.node.style.display="none"
+    }
+}
+
+// class form
+
+class Form{
+
+    constructor(){
+
+        // regroup all HTML error nodes references
+        this.errorNodes = {
+            'firstname' : new errorMessageNode('#prenomError'),
+            'lastname' : new errorMessageNode('#nomError'),
+            'birthdate' : new errorMessageNode('#birthdateError'),
+            'tourney' : new errorMessageNode('#tourneyError'),
+            'locations' : new errorMessageNode('#locationsError'),
+            'conditions' : new errorMessageNode('#conditionsError')
+        }
+
+        // linking each form input to the its validation function
+        this.validationFunctions = {
+            'firstname' : () => checkName('#first'),
+            'lastname' : () => checkName('#last'),
+            'birthdate' : () => checkDate('#birthdate'),
+            'tourney' : () => checkNumber('#quantity'),
+            'locations' : () => checkLocations('location'),
+            'conditions' : () => !document.querySelector('#checkbox1').checked
+        }
+    }
+
+    // is the passed field value a name ?
+    isName(fieldId){
+        const fieldValue = document.querySelector(fieldId).value.trim()
+        const nameRegex =  new RegExp ("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{2,}$")
+        return nameRegex.test(fieldValue)
+    }
+
+    // is the passed field value a number between 0 & 99 ?
+    isBetween_0_and_99(fieldId){
+        const fieldValue = document.querySelector(fieldId).value.trim()
+        const numberRegex = new RegExp ("^[0-9]{1,2}$")
+        return numberRegex.test(fieldValue)
+    }
+
+    // is the passed field value a date ?
+    isDate(fieldId){
+        const fieldValue = document.querySelector(fieldId).value.trim()
+        const dateRegex = new RegExp("^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$")
+        return dateRegex.test(fieldValue)
+    }
+
+    // is one of the radio buttons selected ?
+    isOneLocationChecked(radiosName){
+        const radios = document.getElementsByName(fieldsName)
+
+        for (const radio of radios) {
+            if (radio.checked === true) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    validate(field){
+        this.validationFunctions[input]() === false ? this.errorNodes[input].show() : this.errorNodes[input].hide()
+    }
+}
+
+const myForm = new Form()
